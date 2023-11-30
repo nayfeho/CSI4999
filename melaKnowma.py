3bded430b64c49057f2b39c6bcce2f64170720e8
tdupuy28 <150712394+tdupuy28@users.noreply.github.com>
2023-11-30 17:07:40 -0500

    Update melaKnowma.py
    added photo path editing, separated button functions, and cleaned up code.

--- melaKnowma.py
+++ melaKnowma.py
@@ -1,65 +1,75 @@
-import numpy as np 
+import numpy as np
 import cv2 as cv
-import matplotlib as plt
-from matplotlib import pyplot as plt
 import tensorflow as tf
 import os
-from keras import datasets, layers, models
-from keras.models import Sequential, load_model
-from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from keras.metrics import Precision, Recall, BinaryAccuracy
-import imghdr
+from keras.models import load_model
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
+import ttkbootstrap as ttk
 from PIL import ImageTk, Image
 
-def selectPicture():
-    global img
+
+def browser():
+    filename = filedialog.askopenfilename(
+        initialdir="/images", title="Select Image", filetypes=[
+            ("image", "*.png *.jpg *.jpeg *.bmp")
+        ])
+    path.delete(0, END)
+    path.insert(0, filename)
+    set_photo()
+
+
+def set_photo():
     global picture_path
-    filename = filedialog.askopenfilename(initialdir="/images", title="Select Image",filetypes=[
-                    (".jpg, .jpeg, .png, .bmp", ".jpeg"),
-                    (".jpg, .jpeg, .png, .bmp", ".png"),
-                    (".jpg, .jpeg, .png, .bmp", ".jpg"),
-                    (".jpg, .jpeg, .png, .bmp", ".bmp"),
-                ])
-    img = Image.open(filename)
-    img = img.resize((200,200))
+    global img
+    picture_path = path.get()
+    img = Image.open(picture_path)
+    img = img.resize((256, 256))
     img = ImageTk.PhotoImage(img)
-    show_picture['image'] = img
-    picture_path = filename
+    photo['image'] = img
+
+
+def classify_photo():
+    set_photo()
+    # prediction_label['text'] = "Prediction: TEST"
+    # certainty_label['text'] = "Certainty: 50%"
     if picture_path is not None:
         loadedmodel = load_model(os.path.join('models', 'modelV1.0'))
 
         melanomatestimg = cv.imread(picture_path)
         resizedtest2 = tf.image.resize(melanomatestimg, (256, 256))
-        melanomapred = loadedmodel.predict(np.expand_dims(resizedtest2/255, 0))
+        melanomapred = loadedmodel.predict(np.expand_dims(resizedtest2 / 255, 0))
         print(melanomapred)
         if melanomapred < 0.50:
-            result = round(100*(1-melanomapred[0][0]), 2)
+            result = round(100 * (1 - melanomapred[0][0]), 2)
         else:
-            result = round(100*melanomapred[0][0], 2)
+            result = round(100 * melanomapred[0][0], 2)
         prediction_label['text'] = f"Prediction: {'Benign ' if melanomapred < 0.50 else 'Melanoma '}"
         certainty_label['text'] = f"Certainty: {str(result) + '%'}"
 
 
 window = tk.Tk()
-NAME = "MelaKnowma"
-DEFAULT_SIZE = "600x400"
+NAME = "Mela-Know-ma"
+DEFAULT_SIZE = "500x500"
 window.title(NAME)
 window.geometry(DEFAULT_SIZE)
 
-prediction_label = tk.Label(window)
-certainty_label = tk.Label(window)
-#myLabel= Label(window, text= result ).place(x=50, y=50)
-show_picture = tk.Label(window)
-browse = tk.Button(window, text='Select Image',bg='#ffffff', fg='#000000',)
+path_label = ttk.Label(window, text='Photo Path: ')
+path = ttk.Entry(window)
+path.bind("<Return>", (lambda event: set_photo()))
+browse_button = ttk.Button(window, text='Select Photo', command=browser)
+photo = ttk.Label(window)
+classify_button = ttk.Button(window, text='Identify', command=classify_photo)
+prediction_label = ttk.Label(window)
+certainty_label = ttk.Label(window)
 
-browse['command'] = selectPicture
-
-prediction_label.pack(side=tk.TOP)
-certainty_label.pack(side=tk.TOP)
-show_picture.pack(side=tk.TOP)
-browse.pack(side=tk.TOP)
+path_label.pack()
+path.pack()
+browse_button.pack()
+photo.pack()
+classify_button.pack()
+prediction_label.pack()
+certainty_label.pack()
 
 window.mainloop()
